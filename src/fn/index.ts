0109a73b17378c533a210757c6c623dec27e0004
vvyre <seung.yoon.yu@gmail.com>
2025-06-09 15:53:33 +0900

    feat: add n-ary fn

---
+++ src/fn/index.ts
@@ -0,0 +1,2 @@
+export * from './no-op'
+export * from './n-ary-fn.types'
---
+++ src/fn/n-ary-fn.types.ts
@@ -0,0 +1,3 @@
+export type UnaryFn<A, R> = (arg: A) => R
+export type BinaryFn<A, B, R> = (arg1: A, arg2: B) => R
+export type TernaryFn<A, B, C, R> = (arg1: A, arg2: B, arg3: C) => R
